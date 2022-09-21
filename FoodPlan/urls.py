a87be2fb4508a1cd29cc6a4c4d780e8701e76f0d
Artur <artur_goncharov_97@mail.ru>
2022-09-21 22:29:39 +0300

    setting media

--- FoodPlan/urls.py
+++ FoodPlan/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
 
 from foodplanapp import views
+from . import settings
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
